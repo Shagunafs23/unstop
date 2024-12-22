a20022d26170597f29cd535dabcfbeff8be5c3d9
Shagun Mishra <142713021+Shagunafs23@users.noreply.github.com>
2024-12-22 21:33:02 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true, // For optimized builds
+};
 
 export default nextConfig;
