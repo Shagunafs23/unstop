a5fe1cce1d02004839eb10eff50cb81e80b08a07
Shagun Mishra <142713021+Shagunafs23@users.noreply.github.com>
2024-12-22 13:20:34 +0530

    page.jsx

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,5 +1,5 @@
 import { Suspense } from 'react';
-import YourComponent from '../components/YourComponent';
+import YourComponent from '../_components/page';
 
 export default function DashboardPage() {
   return (
